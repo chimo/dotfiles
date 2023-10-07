aa5de972332f493cc30ae213832634484cea49d0
chimo <chimo@chromic.org>
2023-10-06 21:14:19 -0400

    aerc.sh: sync mailbox after quitting

--- .local/bin/aerc.sh
+++ .local/bin/aerc.sh
@@ -1,4 +1,14 @@
 #!/bin/sh -e
 
+# Launch mail client
 lxc exec aerc -- aerc
+
+# Run this in the background after quitting `aerc`
+(
+    # Sync IMAP in case there were any changes
+    lxc exec mbsync -- sh -c "/etc/periodic/15min/refresh_mail" < /dev/null
+
+    # Force swaybar status update
+    rm -f ~/devel/swaybar/states/mail.state
+)&
 
